1796c159a20fd0722c1678bc6af5c46a74c9087c
DevilsDescendant <86499049+DevilsDescendant@users.noreply.github.com>
2022-01-31 14:12:24 +0100

    Gewinnererkennung der Liga, Punktespeicherung

--- src/modules/LeagueMode.java
+++ src/modules/LeagueMode.java
@@ -3,30 +3,52 @@ package modules;
 import common.YoolooKartenspiel;
 import common.YoolooSpieler;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class LeagueMode {
     private List<YoolooSpieler> players;
     private Ligarunde hinrunde;
     private Ligarunde rueckrunde;
+    private HashMap<YoolooSpieler, Integer> score;
 
 
-    public LeagueMode(List<YoolooSpieler> InitialPlayers){
-        for (YoolooSpieler player:InitialPlayers) {
-            this.players.add(player);
-        }
+    public LeagueMode(List<YoolooSpieler> InitialPlayers) {
+        this.players.addAll(InitialPlayers);
         hinrunde = new Ligarunde(players);
         rueckrunde = new Ligarunde(players);
     }
-    public void StartLeague()
-    {
-        for (Matchup matchup: hinrunde.Matchups
-             ) {
+
+    public void StartLeague() {
+        for (Matchup matchup : hinrunde.Matchups
+        ) {
             YoolooKartenspiel game = new YoolooKartenspiel();
             game.spielerRegistrieren(matchup.player);
             game.spielerRegistrieren(matchup.opponent);
-
+            // starte Spiel
+            //Ergebnisse zu score hinzufügen
+            AddScore(null, 0, null, 0);
+            System.out.println("Winner: " + FindWinner().getName());
         }
+    }
+
+    public YoolooSpieler FindWinner() {
+        YoolooSpieler winner = null;
+        int high = 0;
+        for (YoolooSpieler player : players
+        ) {
+            if (score.get(player) > high) {
+                high = score.get(player);
+                winner = player;
+            }
+        }
+        return winner;
+    }
+
+
+    private void AddScore(YoolooSpieler player, int playerScore, YoolooSpieler opponent, int opponentScore) {
+        score.merge(player, playerScore, Integer::sum);
+        score.merge(opponent, opponentScore, Integer::sum);
     }
 
 }
