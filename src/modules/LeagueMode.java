bda75497753cf86523d317b914a96e893f89fde8
DevilsDescendant <86499049+DevilsDescendant@users.noreply.github.com>
2022-02-01 07:49:52 +0100

    Start Clients, start Server als Ligamodus Registrierung der Punktestände

--- src/modules/LeagueMode.java
+++ src/modules/LeagueMode.java
@@ -1,10 +1,15 @@
 package modules;
 
+import client.YoolooClient;
 import common.YoolooKartenspiel;
 import common.YoolooSpieler;
+import server.YoolooServer;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
+
+import static server.YoolooServer.GameMode.GAMEMODE_PLAY_LIGA;
 
 public class LeagueMode {
     private List<YoolooSpieler> players;
@@ -20,16 +25,42 @@ public class LeagueMode {
     }
 
     public void StartLeague() {
-        for (Matchup matchup : hinrunde.Matchups
+        StartRunde(hinrunde);
+        StartRunde(rueckrunde);
+        Finalize();
+    }
+
+    private void StartRunde(Ligarunde runde) {
+        for (Matchup matchup : runde.Matchups
         ) {
             YoolooKartenspiel game = new YoolooKartenspiel();
             game.spielerRegistrieren(matchup.player);
             game.spielerRegistrieren(matchup.opponent);
-            // starte Spiel
-            //Ergebnisse zu score hinzufügen
-            AddScore(null, 0, null, 0);
-            System.out.println("Winner: " + FindWinner().getName());
+            startLigaServer();
+            startLigaClient();
+            AddScore(matchup.player, matchup.opponent);
+            System.out.println("Winner Hinrunde: " + FindWinner().getName());
         }
+    }
+
+    public void startLigaServer(){
+        YoolooServer server = new YoolooServer(44137,2,GAMEMODE_PLAY_LIGA);
+        server.startServer();
+    }
+
+    public void startLigaClient(){
+        Scanner hostnameScanner = new Scanner(System.in);
+        String hostname = hostnameScanner.nextLine();
+
+        YoolooClient client1 = new YoolooClient(hostname, 44137);
+        YoolooClient client2 = new YoolooClient(hostname, 44137);
+        client1.startClient();
+        client2.startClient();
+    }
+
+    private void Finalize(){
+        System.out.println("Winner Total: " + FindWinner().getName());
+        score.clear();
     }
 
     public YoolooSpieler FindWinner() {
@@ -46,9 +77,9 @@ public class LeagueMode {
     }
 
 
-    private void AddScore(YoolooSpieler player, int playerScore, YoolooSpieler opponent, int opponentScore) {
-        score.merge(player, playerScore, Integer::sum);
-        score.merge(opponent, opponentScore, Integer::sum);
+    private void AddScore(YoolooSpieler player, YoolooSpieler opponent) {
+        score.merge(player, player.getPunkte(), Integer::sum);
+        score.merge(opponent, opponent.getPunkte(), Integer::sum);
     }
 
 }
